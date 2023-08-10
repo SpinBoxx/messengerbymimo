b267858e9acdbb97ea0882cce45588ff79c728cf
SpinBoxx <quentin.mimault@orange.fr>
2023-08-10 22:30:07 +0200

    [FEAT] Added route for login + Added github/google connection buttons

---
+++ app/api/register/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import prismadb from "@/app/lib/prismadb";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { email, name, password } = body;
+
+  if (!email)
+    return NextResponse.json({ message: "Email is required" }, { status: 400 });
+  if (!name)
+    return NextResponse.json({ message: "Name is required" }, { status: 400 });
+  if (!password)
+    return NextResponse.json(
+      { message: "Password is required" },
+      { status: 400 }
+    );
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await prismadb.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+  return NextResponse.json(user, { status: 201 });
+}
--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Github, X } from "lucide-react";
 import {
   Form,
   FormControl,
@@ -15,6 +16,7 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { Separator } from "./ui/separator";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -78,7 +80,11 @@ export default function AuthForm() {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter your email here..." {...field} />
+                    <Input
+                      type="email"
+                      placeholder="Enter your email here..."
+                      {...field}
+                    />
                   </FormControl>
 
                   <FormMessage />
@@ -108,6 +114,28 @@ export default function AuthForm() {
             </Button>
           </form>
         </Form>
+        <div>
+          <div className="my-4 flex flex-row items-center justify-between text-center">
+            <Separator className="w-2/5 justify-start" /> Or
+            <Separator className="w-2/5 justify-self-end" />
+          </div>
+          <div className="flex justify-around">
+            <Button className="w-1/3 bg-slate-800">
+              <Github className="mr-2 h-5 w-5" /> Github
+            </Button>
+            <Button className="w-1/3 bg-slate-800">
+              <svg
+                className="mr-2 h-5 w-5 fill-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+              </svg>
+              Google
+            </Button>
+          </div>
+        </div>
+
         <div className="mt-6 flex justify-center gap-2 px-2 text-sm text-gray-500">
           <span>
             {variant === "LOGIN"

3dd52ead7cee82dac37d264e7fce073db925119e
SpinBoxx <quentin.mimault@orange.fr>
2023-08-21 23:16:17 +0200

    [FIX] Fix useConversation hook

--- hooks/use-conversation.ts
+++ hooks/use-conversation.ts
@@ -4,10 +4,10 @@ import { useMemo } from "react";
 const useConversation = () => {
   const params = useParams();
   const conversationId = useMemo(() => {
-    if (params?.conversationId) {
+    if (!params?.conversationId) {
       return "";
     }
-    return params?.conversationId as string;
+    return params.conversationId as string;
   }, [params?.conversationId]);
 
   const isOpen = useMemo(() => !!conversationId, [conversationId]);
